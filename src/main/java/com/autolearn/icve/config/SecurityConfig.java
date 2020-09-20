8680c62cf23ac94838c5b706ad96d5c05afed10e
h1525 <1525947163@qq.com>
2020-09-20 11:15:12 +0800

    添加无需拦截的接口

--- src/main/java/com/autolearn/icve/config/SecurityConfig.java
+++ src/main/java/com/autolearn/icve/config/SecurityConfig.java
@@ -46,7 +46,6 @@ public class SecurityConfig implements WebMvcConfigurer {
         registry.addInterceptor(loginInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns(noLoginUrl);
-
     }
 
     @Override
